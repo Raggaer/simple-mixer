79245e1820e899bf9bb13620e58b980f87ab15be
Raggaer <nakotoffana@gmail.com>
2022-12-04 22:42:55 +0100

    Add more tests

--- hardhat/test/mix-test.js
+++ hardhat/test/mix-test.js
@@ -21,7 +21,7 @@ describe("SimpleMixer test", function () {
     const startingBalance3 = await ethers.provider.getBalance(addr3.address);
     const startingBalanceFee = await ethers.provider.getBalance(owner.address);
 
-    const [v, s] = await generateMainServerSignature(owner, mixer.address, addr3.address);
+    const [v, s] = await generateMainServerSignature(owner, mixer.address, addr3.address, "5.0");
     await mixer.connect(addr2).withdraw(v, s);
 
     // Check addr3 balance
@@ -52,7 +52,7 @@ describe("SimpleMixer test", function () {
     const startingBalance3 = await ethers.provider.getBalance(addr3.address);
     const startingBalanceFee = await ethers.provider.getBalance(owner.address);
 
-    const [v, s] = await generateMainServerSignature(owner, mixer.address, addr3.address);
+    const [v, s] = await generateMainServerSignature(owner, mixer.address, addr3.address, "5.0");
     await mixer.connect(addr2).withdraw(v, s);
 
     // Check addr3 balance
@@ -65,10 +65,60 @@ describe("SimpleMixer test", function () {
 
     await expect(mixer.connect(addr2).withdraw(v, s)).to.be.revertedWith("Salt already used");
   });
+
+  it("Use a signature signed by other invalid key", async function () {
+    const [owner, addr1, addr2, addr3, addr4] = await ethers.getSigners();
+
+    const mixerFactory = await ethers.getContractFactory("SimpleMixer");
+    const mixer = await mixerFactory.deploy(owner.address, 1000);
+    await mixer.deployed();
+
+    // User 1 deposits some coins
+    await mixer.connect(addr1).deposit({
+      value: ethers.utils.parseEther("5.0")
+    });
+
+    expect(await mixer.getBalance()).to.equal(ethers.utils.parseEther("5.0"));
+
+    // User now has a signature from the main server
+    // addr2 sends the transaction and addr3 should get the mixer coins
+    const startingBalance3 = await ethers.provider.getBalance(addr3.address);
+    const startingBalanceFee = await ethers.provider.getBalance(owner.address);
+
+    const [v, s] = await generateMainServerSignature(addr4, mixer.address, addr3.address, "5.0");
+    await expect(mixer.connect(addr2).withdraw(v, s)).to.be.revertedWith("Invalid provided signature");
+  });
+
+  it("Use a different amount at withdraw", async function () {
+    const [owner, addr1, addr2, addr3, addr4] = await ethers.getSigners();
+
+    const mixerFactory = await ethers.getContractFactory("SimpleMixer");
+    const mixer = await mixerFactory.deploy(owner.address, 1000);
+    await mixer.deployed();
+
+    // User 1 deposits some coins
+    // For this test we deposit 15 ETH but the user should withdraw 5
+    await mixer.connect(addr1).deposit({
+      value: ethers.utils.parseEther("15.0")
+    });
+
+    expect(await mixer.getBalance()).to.equal(ethers.utils.parseEther("15.0"));
+
+    // User now has a signature from the main server
+    // addr2 sends the transaction and addr3 should get the mixer coins
+    const startingBalance3 = await ethers.provider.getBalance(addr3.address);
+    const startingBalanceFee = await ethers.provider.getBalance(owner.address);
+
+    const [v, s] = await generateMainServerSignature(owner, mixer.address, addr3.address, "5.0");
+
+    // User sends on the value 10 instead of the signed 5
+    v.amount = v.amount.add(ethers.utils.parseEther("5.0")); 
+    await expect(mixer.connect(addr2).withdraw(v, s)).to.be.revertedWith("Invalid provided signature");
+  });
 });
 
 // Generates an EIP-712 valid signature
-async function generateMainServerSignature(signer, _contract, _to) {
+async function generateMainServerSignature(signer, _contract, _to, _value) {
   const domain = {
     name: "SimpleMixer",
     verifyingContract: _contract
@@ -84,7 +134,7 @@ async function generateMainServerSignature(signer, _contract, _to) {
   };
 
   const value = {
-    amount: ethers.utils.parseEther("5.0"),
+    amount: ethers.utils.parseEther(_value),
     deadline: 2670810385,
     salt: 420,
     to: _to,
